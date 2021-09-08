0db7ab4860de31b304753d1ef48cc87348dfeb24
구민구 <corleone@kakao.com>
2021-09-09 00:25:16 +0900

    Create stream_test.py

---
+++ utils/stream_test.py
@@ -0,0 +1,15 @@
+﻿# stream test program
+
+import camelot
+
+path = "./pdf_for_stream/"
+filename = "8"
+tables = camelot.read_pdf(path+filename+".pdf", flavor="stream", page="1")
+
+for t in tables:
+    print("table exist")
+print(tables[0].df)
+camelot.plot(tables[0], kind='textedge').show()
+
+
+
