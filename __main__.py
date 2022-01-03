0224d438efbdb10dfb30cd4c1c6e8994cbadb965
Ji-yong219 <wldydslapjyy@naver.com>
2022-01-03 18:27:38 +0900

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -22,4 +22,4 @@ if __name__ == "__main__":
     fh.setFormatter(formatter)
     logger.addHandler(fh)
 
-    app.run(host="127.0.0.1", port=5001, use_reloader=False, debug = True)
+    app.run(host= "127.0.0.1", port= 5001, use_reloader= False, debug= True)
